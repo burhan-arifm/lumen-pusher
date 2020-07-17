a2a7bb27ef2cd5fd57b1f401fd1c8c2ef1dc177c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-17 18:48:39 +0000

    Deploy burhan-arifm/lumen-pusher to github.com/burhan-arifm/lumen-pusher.git:gh-pages

---
+++ precache-manifest.dd9a0d3f614f1d2ed1bf2c980abb605a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc1463275d68da82722b1b14413a989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ffa2f342fe051796d59",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87dde18a982ba6a5f211",
+    "url": "/static/js/2.b98720ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b98720ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffa2f342fe051796d59",
+    "url": "/static/js/main.dfbafcf8.chunk.js"
+  },
+  {
+    "revision": "553a4d9493759350ccf8",
+    "url": "/static/js/runtime-main.cfd9d63b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
